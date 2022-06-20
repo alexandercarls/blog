575297c3c1a56138719fb0ab43e72ed184692359
Alexander Carls <alexandercarls@icloud.com>
2022-06-20 23:07:14 +0200

    Heading component

--- build.cljs
+++ build.cljs
@@ -1,9 +1,11 @@
 (ns build
   (:require ["zx" :refer [glob fs]]
-            ["@markdoc/markdoc$default" :as markdoc]
+            ["@markdoc/markdoc$default" :as markdoc :refer [Tag]]
+            ["@sindresorhus/slugify$default" :as slugify]
             ["path" :as path]
             ["react$default" :as React]
             [reagent.dom.server :as srv]
+            [reagent.core :as r]
             [nbb.core :refer [slurp await]]
             [clojure.string :as str]
             [clojure.edn :as edn]
@@ -21,19 +23,34 @@
   (when-let [frontmatter (j/get-in ast [:attributes :frontmatter])]
     (edn/read-string frontmatter)))
 
-(defn post-layout [date content]
-  [:article.relative.pt-8.mt-6
-   [:div.text-sm.leading-6
-    [:dl
-     [:dd.absolute.top-0.inset-x-0.text-slate-700
-      [:time {:date-time (.toISOString date)} (date->human date)]]]]
-   [:div.prose.prose-slate.max-w-none content]])
+(defn Heading [{:keys [id level children]}]
+  (let [heading-tag (if (= level 1)
+                      [:h1.not-prose.text-2xl.font-extrabold.tracking-tight.text-slate-900.md:text-3xl {:id id} children]
+                      [(keyword (str "h" level)) {:id id :class "text-lg md:text-2xl"} children])]
+    [:a.no-underline.relative {:href (str "#" id)} heading-tag]))
+
+(def node-heading {:render "Heading"
+                   :children ["inline"]
+                   :attributes {:id {:type 'String}
+                                :level {:type 'Number :required true :default 1}}
+                   :transform (fn [node config] (let [attributes (.transformAttributes node config)
+                                                      children (.transformChildren node config)
+                                                      id (slugify (first children))]
+                                                  (Tag.
+                                                   "Heading"
+                                                   (clj->js (into (js->clj attributes) {:id id}))
+                                                   children)))})
 
 (defn markdown-to-react-elements [markdown]
   (let [ast (markdoc/parse markdown)
         frontmatter (parse-fronmatter ast)
-        rendertree (markdoc/transform ast (clj->js {:variables frontmatter :functions {:toHumanDate md-to-human-date}}))
-        react-elements (markdoc/renderers.react rendertree React)]
+        rendertree (markdoc/transform ast (clj->js {:variables frontmatter
+                                                    :functions {:toHumanDate md-to-human-date}
+                                                    :nodes {:heading node-heading}}))
+        react-elements (markdoc/renderers.react
+                        rendertree
+                        React
+                        (clj->js {:components {"Heading" (r/reactify-component Heading)}}))]
     [react-elements frontmatter]))
 
 (defn make-templated-html [title content]
@@ -73,12 +90,10 @@
                               (fs.copy (path/dirname post-path) destfolder)
                               (fs.remove (path/join destfolder "index.md"))
                               (fs.writeFile (path/join destfolder "index.html") (:html p))))) posts))]
-    (prn posts)
     posts))
 
 (build)
 
 (comment
-  (markdown-to-html "# Test")
-  (await (build))
-  )
+  (markdown-to-react-elements "# Test")
+  (await (build)))
