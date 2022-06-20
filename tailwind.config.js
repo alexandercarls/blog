0d8925aff98d66a9fb1b102027d432eb5a966405
Alexander Carls <alexandercarls@icloud.com>
2022-06-20 17:09:05 +0200

    Add Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/**/*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+   require('@tailwindcss/typography')
+  ],
+}
